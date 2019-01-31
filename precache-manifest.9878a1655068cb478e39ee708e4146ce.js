d810916f05541eae1c0cbe78668ed04c826fe18f
Amy Silva <amyambard@icloud.com>
2019-01-30 17:53:01 -0800

    Updates

---
+++ precache-manifest.9878a1655068cb478e39ee708e4146ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "b8f7ee9ce4fb616a039d",
+    "url": "/portfolio/static/js/main.b8f7ee9c.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/portfolio/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "b8f7ee9ce4fb616a039d",
+    "url": "/portfolio/static/css/main.454d24f7.chunk.css"
+  },
+  {
+    "revision": "a8205ea2f5353e899f2fdbcd7c846292",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f3a2ffd088f5ab11eccc4e883dece3dd.js"
+  "/portfolio/precache-manifest.9878a1655068cb478e39ee708e4146ce.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
