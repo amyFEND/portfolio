ab45e950760daaaa7fe02e54efe8803872d5d6d3
Amy Silva <amyambard@icloud.com>
2019-03-07 19:26:36 -0800

    Fix: carousel ui
    - Page "jumps" around page when prev/next button clicked
    - Next/prev icons do not accept custom color

--- src/App/components/Carousel/Carousel.js
+++ src/App/components/Carousel/Carousel.js
@@ -6,31 +6,39 @@ import {ReactComponent as UdaciLogo} from './Assets/udacity.svg';
 class Carousel extends Component {
   render() {
     return(
-      <div id="carouselIndicators" class="carousel slide" data-ride="carousel">
+      <div id="carouselIndicators" class="carousel slide" data-ride="carousel" data-interval="3000" data-pause="hover">
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <UdaciLogo className="udacilogo" alt="First slide" />
-            {/* <svg class="placeholder" width="200px" height="300px" alt="First slide" ></svg> */}
+            {/* <UdaciLogo className="udacilogo" alt="First slide" /> */}
+            <svg class="placeholder" width="200px" height="300px" alt="First slide" ></svg>
           </div>
+
           <div class="carousel-item">
+            {/* <UdaciLogo className="udacilogo" alt="Second slide" /> */}
             <svg class="placeholder" width="200px" height="300px" alt="Second slide" />
           </div>
+
           <div class="carousel-item">
+            {/* <UdaciLogo className="udacilogo" alt="Third slide" /> */}
             <svg class="placeholder" width="200px" height="300px" alt="Third slide" />
           </div>
+
         </div>
+
         <a class="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true" />
           <span class="sr-only">Previous</span>
         </a>
+
         <a class="carousel-control-next" href="#carouselIndicators" role="button" data-slide="next">
           <span class="carousel-control-next-icon" aria-hidden="true" />
           <span class="sr-only">Next</span>
         </a>
+
         <ol class="carousel-indicators">
-        <li data-target="#carouselIndicators" data-slide-to="0" class="active" />
-        <li data-target="#carouselIndicators" data-slide-to="1" />
-        <li data-target="#carouselIndicators" data-slide-to="2" />
+          <li data-target="#carouselIndicators" data-slide-to="0" class="active" />
+          <li data-target="#carouselIndicators" data-slide-to="1" />
+          <li data-target="#carouselIndicators" data-slide-to="2" />
         </ol>
       </div>
     )
